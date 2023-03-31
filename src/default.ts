fc0e4622befa571369367566d8b4472c45feddb5
DavidFeng13 <56263241+DavidFeng13@users.noreply.github.com>
2023-04-01 00:20:30 +0800

    up
    up

---
+++ src/default.ts
@@ -0,0 +1,12 @@
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+- 回复 **ApiKey** 获取最新的ApiKey。
+- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
